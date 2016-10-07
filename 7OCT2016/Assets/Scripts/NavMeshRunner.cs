82475f3595c9ca711d44f108ef9257d17b1be699
Evan <daley.evan@gmail.com>
2016-10-07 16:52:10 -0700

    Improve the color scheme

---
+++ 7OCT2016/Assets/Scripts/NavMeshRunner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavMeshRunner : MonoBehaviour {
+
+	public Transform target;
+	private NavMeshAgent NavMeshAgent;
+
+	void Start () 
+	{
+		NavMeshAgent = GetComponent<NavMeshAgent> ();
+		NavMeshAgent.updateRotation = false;
+	}
+	
+	void Update () 
+	{
+		NavMeshAgent.SetDestination (target.transform.position);
+	}
+}
